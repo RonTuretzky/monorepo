f14b376a70892a208a009a8f2d8370484b497bbd
Ron Turetzky <turetzkyron@gmail.com>
2025-02-05 19:30:43 +0200

    chore: cleanup

--- restaking/src/bin/init_eigenstaking.rs
+++ restaking/src/bin/init_eigenstaking.rs
@@ -9,16 +9,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
     println!("Initializing EigenStaking client...");
     println!("\nConfiguration:");
-    println!("HTTP Endpoint: https://withered-convincing-meadow.quiknode.pro/89fd706450ed0a8279f87c01e52ae78d9b308ce7");
-    println!("WS Endpoint: wss://withered-convincing-meadow.quiknode.pro/89fd706450ed0a8279f87c01e52ae78d9b308ce7");
+    println!("HTTP Endpoint: https://eth.llamarpc.com");
+    println!("WS Endpoint: wss://eth.drpc.org");
     println!("Registry Coordinator Address: 0xeCd099fA5048c3738a5544347D8cBc8076E76494");
     println!("Operator State Retriever Address: D5D7fB4647cE79740E6e83819EFDf43fa74F8C31");
     println!("Registry Coordinator Deploy Block: 20227142");
     
     // Initialize the EigenStaking client with default values
     let client = EigenStakingClient::new(
-        String::from("https://withered-convincing-meadow.quiknode.pro/89fd706450ed0a8279f87c01e52ae78d9b308ce7"),
-        String::from("wss://withered-convincing-meadow.quiknode.pro/89fd706450ed0a8279f87c01e52ae78d9b308ce7"),
+        String::from("https://eth.llamarpc.com"),
+        String::from("wss://eth.drpc.org"),
         address!("0xeCd099fA5048c3738a5544347D8cBc8076E76494").into(),
         20227142,
     ).await?;
